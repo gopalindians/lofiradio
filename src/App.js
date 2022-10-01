6742c202a3695aff860aabfc0bd5dd9b0219640a
Gopal Sharma <gopalsharma@Gopals-iMac.local>
2022-10-02 01:04:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,170 @@
-import logo from './logo.svg';
 import './App.css';
+import YouTube from "react-youtube";
+import Player from "./Player";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [videoIds] = useState([
+        {
+            id: 'rtTI1rh9U5M',
+            title: '1 Hour Of Night Hindi Lofi Songs To Study \\Chill \\Relax \\Refreshing',
+            poster: 'rtTI1rh9U5M.webp',
+            author: 'Mood Creations'
+        },
+        {
+            id: 'q0BVR5jRXxE',
+            title: 'Bollywood Lofi Mixtape Vol.1 | 🎶 30 Minute Mix to Relax, Drive, Study, Chill 🌌| WORMONO',
+            poster: 'q0BVR5jRXxE.webp',
+            author: 'Wormono Music'
+        },
+        {
+            id: 'Fs7CbWEES88',
+            title: '45 Minutes of lofi songs -Bollywood Songs Hindi Songs #lofi #bollywood #bollywoodsongs',
+            poster: 'Fs7CbWEES88.webp',
+            author: 'Wormono Music'
+        },
+        {
+            id: 'CzWsjMkOYKA',
+            title: '1hour lofi mashup sleeping| relaxing|study|work|@Lofi ms lover feel the music..',
+            poster: 'CzWsjMkOYKA.webp',
+            author: 'Lofi ms lover'
+        },
+        {
+            id: 'uS5wfG1REN4',
+            title: '1 Hour Of Night Hindi Lofi Songs To Study, Chill, Relax and Refreshing',
+            poster: 'uS5wfG1REN4.webp',
+            author: 'LoFi and Chill'
+        }
+    ]);
+    const randomVideo = () => {
+        let random = videoIds[(Math.floor(Math.random() * videoIds.length))];
+        setCurrentVideoId(random.id)
+        setCurrentVideoData(random)
+        console.debug('RANDOM VIDEO');
+        return random.id;
+    }
+    const [currentVideoData, setCurrentVideoData] = useState(
+        {
+            id: '',
+            poster: '',
+            author: ''
+        });
+    let random = videoIds[(Math.floor(Math.random() * videoIds.length))];
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentVideoId, setCurrentVideoId] = useState(random.id);
+    const [iframeTarget, setIframeTarget] = useState(Event.target);
+    const [progress, setProgress] = useState(0);
+    const [videoTitle, setVideoTitle] = useState('');
+    const [videoLength, setVideoLength] = useState(100);
+    const opts = {
+        height: 1,
+        width: 1,
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+
+
+    useEffect(() => {
+        setCurrentVideoData(random);
+    }, []);
+
+    useEffect(() => {
+        let timeoutTimer = '';
+        let intervalTimer = '';
+        timeoutTimer = setTimeout(() => {
+            intervalTimer = setInterval(() => {
+                if (iframeTarget !== undefined) {
+                    if (iframeTarget.getCurrentTime() !== 0) {
+                        if (!isNaN(iframeTarget.getCurrentTime() / 60)) {
+                            setProgress(iframeTarget.getCurrentTime() / 60)
+                        }
+                    }
+                }
+            }, 300);
+        }, 200);
+
+        return (() => {
+            clearInterval(intervalTimer);
+            clearTimeout(timeoutTimer);
+        })
+
+    }, [iframeTarget]);
+
+    const clickPlay = () => {
+        iframeTarget.playVideo();
+        setIsPlaying(true);
+    }
+    const clickPause = () => {
+        iframeTarget.pauseVideo();
+        setIsPlaying(false);
+    }
+
+    const playRandom = () => {
+        setCurrentVideoId(randomVideo);
+        setTimeout(() => {
+            iframeTarget.clearVideo();
+            iframeTarget.loadVideoById(currentVideoId);
+            iframeTarget.playVideo();
+            setVideoLength(iframeTarget.getDuration() / 60);
+            setIsPlaying(true);
+        }, 200);
+
+    }
+
+    const handleNext = () => {
+        setCurrentVideoId(randomVideo);
+        iframeTarget.clearVideo();
+        iframeTarget.loadVideoById(currentVideoId);
+        iframeTarget.playVideo();
+        setIsPlaying(true);
+    }
+
+    const handlePrev = () => {
+        setCurrentVideoId(randomVideo);
+        iframeTarget.clearVideo();
+        iframeTarget.loadVideoById(currentVideoId);
+        iframeTarget.playVideo();
+        setIsPlaying(true);
+    }
+    const _onReady = (event) => {
+        setIframeTarget(event.target);
+        setVideoTitle(event.target.videoTitle);
+        setVideoLength(event.target.getDuration() / 60);
+        setIsPlaying(true);
+    }
+
+    const seekTo = (time) => {
+        iframeTarget.seekTo(time * 60);
+    }
+
+    const playRepeat = (time) => {
+        iframeTarget.setLoop(true);
+    }
+    return (
+        <>
+            <Player
+                currentVideoData={currentVideoData}
+                isPlaying={isPlaying}
+                clickPlay={clickPlay}
+                clickPause={clickPause}
+                progress={progress}
+                videoLength={videoLength}
+                playRandom={playRandom}
+                handleNext={handleNext}
+                handlePrev={handlePrev}
+                seekTo={seekTo}
+                playRepeat={playRepeat}
+            />
+            <div style={{
+                opacity: 0,
+            }}>
+                <YouTube videoId={currentVideoId} opts={opts} onReady={_onReady}/>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Player.js
@@ -0,0 +1,179 @@
+import {useState} from "react";
+
+import('./Player.css');
+
+export default function Player({
+                                   currentVideoData,
+                                   isPlaying,
+                                   clickPlay,
+                                   clickPause,
+                                   progress,
+                                   videoLength,
+                                   playRandom,
+                                   handleNext,
+                                   handlePrev,
+                                   seekTo,
+                                   playRepeat
+                               }) {
+    const [seek, setSeek] = useState(progress);
+
+    const handleChange = e => {
+        setSeek(parseFloat(e.target.value).toFixed(2));
+        seekTo(e.target.value * (videoLength / 60));
+    };
+
+    const handlePlayPause = () => {
+        if (isPlaying) {
+            clickPause();
+        } else {
+            clickPlay();
+        }
+    }
+    return (
+        <div className='player' style={{
+            zIndex: 999999
+        }}>
+            <div className='player_inner'>
+                <div className='player_inner__top'>
+                    <div className='t_left'>
+                        <i className='fa fa-bars' style={{
+                            opacity:0
+                        }}></i>
+                    </div>
+                    <div className='t_mid'>
+                        <h1>Lofi Radio</h1>
+                    </div>
+                    <div className='t_right'>
+                        <i className='fa fa-search'
+                           style={{
+                               opacity:0
+                           }}></i>
+                    </div>
+                </div>
+                <div className='player_inner__middle'>
+                    {/*<input className='trigger--4' name='omni' type='radio'/>*/}
+                    <input className='trigger--3' name='omni' type='radio'/>
+                    <input className='trigger--2' name='omni' type='radio'/>
+                    {/*<input className='trigger--1' name='omni' type='radio'/>*/}
+                    <input className='empty'/>
+                    <div className='cube'>
+                        <div className='cube_inner'>
+                            <div className='cube_inner__front'>
+                                <div className='bars' style={{
+                                    opacity: isPlaying ? 1 : 0
+                                }}>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                    <div className='bars_bar'></div>
+                                </div>
+                                <div className='details'>
+                                    <div className='details_album'
+                                         style={{
+                                             background: currentVideoData.poster!==''?`url(${require('./posters/'+currentVideoData.poster)}) no-repeat center`:'' ,
+                                             // backgroundImage: `url(${currentVideoData.poster})`
+                                             // backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")",
+                                         }}></div>
+                                    <h2>{currentVideoData.title}</h2>
+                                    <h3>{currentVideoData.author}</h3>
+                                </div>
+                            </div>
+                            <div className='cube_inner__left'>
+                                <div className='options'>
+                                    <i className='fa fa-headphones'></i>
+                                    <i className='fa fa-redo-alt'></i>
+                                    <i className='fa fa-random'></i>
+                                    <i className='fa fa-fast-forward'></i>
+                                    <i className='fa fa-music'></i>
+                                </div>
+                            </div>
+                            <div className='cube_inner__right'>
+                                <div className='volume'>
+
+                                    <input type='range' max="100" min="10">
+                                    </input>
+
+                                    <i className='fa fa-volume-up'></i>
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+                                    {/*<div className='volume_pip'></div>*/}
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='r_trim'></div>
+                    <div className='l_trim'></div>
+                    <div className='shadow_right'></div>
+                    <div className='shadow_left'></div>
+                </div>
+                <div className='player_inner__bottom'>
+                    <div className='options'>
+                        <i className='fa fa-repeat' onClick={playRepeat}></i>
+                        <i className='fa fa-random' onClick={playRandom} title="Play random"></i>
+                    </div>
+                    <div className='playbar'>
+                        <input type="range" id="seek-slider" max={videoLength} min="0" value={seek}
+                               className='playbar_inner' style={{
+                            marginTop: '-1px',
+                            width: '100%'
+                        }} onChange={e => setSeek(e.target.value)}
+                               onMouseUp={handleChange}/>
+                        {/*<div className='playbar_inner' style={{width: progress + '%'}}>*/}
+                        {/*    <div style={{*/}
+                        {/*        float: 'right',*/}
+                        {/*        marginTop: '-8px',*/}
+                        {/*        color: '#44ab87'*/}
+                        {/*    }}>*/}
+                        {/*        <i className="fa fa-circle" aria-hidden="true"></i>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+                        <div className='playbar_left'>
+                            {progress !== 0
+                                ? <span>{parseFloat(progress.toFixed(2)).toString().replace('.', ':')}</span>
+                                : <span>0:00</span>
+                            }
+                        </div>
+                        <div className='playbar_right'>
+                            {videoLength !== 0
+                                ? <span>{parseFloat(videoLength.toFixed(2)).toString().replace('.', ':')}</span>
+                                : <span>100:00</span>
+                            }
+                        </div>
+                    </div>
+                    <div className='controls'>
+                        <div className='controls_left'>
+                            <i className='fa fa-step-backward' onClick={handlePrev} title="Play prev"></i>
+                        </div>
+                        <div className='controls_middle'>
+                            {isPlaying
+                                ? <i className='fa fa-pause-circle' onClick={handlePlayPause}></i>
+                                : <i className='fa fa-play-circle' onClick={handlePlayPause}></i>
+                            }
+                        </div>
+                        <div className='controls_right'>
+                            <i className='fa fa-step-forward' onClick={handleNext} title="Play next"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
